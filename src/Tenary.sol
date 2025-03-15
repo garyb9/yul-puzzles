cdc29492ddf349b1b8f3103fbcb7939a21e38150
garyb9 <gberkinfand@gmail.com>
2025-03-15 21:29:24 +0200

    arrays

--- src/Tenary.sol
+++ src/Tenary.sol
@@ -10,6 +10,16 @@ contract Tenary {
             // store `10` in the storage variable `result` if a > b,
             // else if b > c, store `20`,
             // else, store `30`
+            if gt(a, b) { // a > b
+                sstore(result.slot, 0x0a)
+                return(0, 0)
+            }
+            if gt(b, c) { // b > c
+                sstore(result.slot, 0x14)
+                return(0, 0)
+            }
+            // else
+            sstore(result.slot, 0x1e)
         }
     }
 }
--- src/WriteToFixedArray.sol
+++ src/WriteToFixedArray.sol
@@ -9,6 +9,14 @@ contract WriteToFixedArray {
             // your code here
             // store the values in the fixed array `x` in the storage variable `writeHere`
             // Hint: https://www.rareskills.io/post/solidity-dynamic
+
+            let data := x // pointer directly to the first element of the fixed array
+            
+            // iterate exactly 5 times
+            for { let i := 0 } lt(i, 5) { i := add(i, 1) } {
+                let element := mload(add(data, mul(i, 0x20)))
+                sstore(add(writeHere.slot, i), element)
+            }
         }
     }
 
