e80aa126bbc344125a6cf4c6ba42ef2e1dcc82bd
garyb9 <gberkinfand@gmail.com>
2025-03-15 18:17:03 +0200

    revet panic

--- src/RevertWithPanic.sol
+++ src/RevertWithPanic.sol
@@ -8,6 +8,11 @@ contract RevertWithPanic {
             // revert the function with an error of type `Panic(uint256)`
             // use "0x01" as error code
             // Hint: The error type is built-in and cannot be re-defined. See https://www.rareskills.io/post/try-catch-solidity
+
+            mstore(0x00, 0x4e487b7100000000000000000000000000000000000000000000000000000000) // padded selector
+            mstore(0x04, 0x1) // uint256 arg
+
+            revert(0x00, 0x24) // 4 bytes selector + 32 bytes uint256 = 36 (0x24) bytes total
         }
     }
 }
--- src/RevertWithSelectorPlusArgs.sol
+++ src/RevertWithSelectorPlusArgs.sol
@@ -5,14 +5,13 @@ contract RevertWithSelectorPlusArgs {
     error RevertData(uint256); // selector: 0xae412287
 
     function main(uint256 x) external pure {
-
         assembly {
             // your code here
             // revert custom error with x parameter
             // Hint: concatenate selector and x by storing them
             // adjacent to each other in memory
             mstore(0x00, 0xae41228700000000000000000000000000000000000000000000000000000000) // padded selector
-            mstore(0x04, x)  // uint256 arg
+            mstore(0x04, x) // uint256 arg
 
             revert(0x00, 0x24) // 4 bytes selector + 32 bytes uint256 = 36 (0x24) bytes total
         }
